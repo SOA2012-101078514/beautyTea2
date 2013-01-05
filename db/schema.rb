a509441d0ecb674c4391b44d81c1ef196908ad98
Andy Phil <philandy01@yahoo.fr>
2013-01-06 02:45:25 +0800

    db

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130105075439) do
-
-  create_table "bulletins", :force => true do |t|
-    t.string   "msg"
-    t.date     "date"
-    t.date     "deadline"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20130101081138) do
 
   create_table "users", :force => true do |t|
     t.string   "provider"
